8c8aeb610e29e3274115acc518545f350dd6be46
nari <snaritada@gmail.com>
2020-07-26 04:48:29 +0900

    test

--- assets/js/likes.js
+++ assets/js/likes.js
@@ -205,6 +205,7 @@
         img = sizes[img];
         thumbnail.url = img.url;
         thumbnail.height = img.height;
+        ctx.url = post.photos[0].url;
         return thumbnail.width = img.width;
       }
     };
